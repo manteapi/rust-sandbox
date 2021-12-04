b76c4c9e913701587e56175b65af2404b5c0152d
Pierre-Luc Manteaux <manteaux@anatoscope.com>
2021-12-04 23:15:47 +0100

    solved day1

--- adventofcode/src/main.rs
+++ adventofcode/src/main.rs
@@ -3,11 +3,19 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
-    if let Ok(lines) = read_lines("../day/1/input") {
-        let lines_as_string: io::Result<Vec<String>> = lines.collect();
-        for line in lines_as_string {
-            println!("{:?}", line)
+    let result = parse_input("../day/1/input");
+    match result {
+        Ok(input) => {
+            println!(
+                "{:?}",
+                compute_orientation(input)
+                    .iter()
+                    .filter(|&&orientation| return orientation == true)
+                    .collect::<Vec<&bool>>()
+                    .len()
+            );
         }
+        Err(error) => println!("{:?}", error),
     }
 }
 
@@ -17,4 +25,40 @@ where
 {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
+}
+
+fn compute_orientation(input: Vec<i32>) -> Vec<bool> {
+    let mut output: Vec<bool> = Vec::new();
+    for (index, number) in input.iter().enumerate() {
+        if index > 0 {
+            let previous_index: usize = index - 1;
+            let previous_number: i32 = input[previous_index];
+            let orientation: bool = (number > &previous_number) as bool;
+            output.push(orientation);
+        }
+    }
+    return output;
+}
+
+fn parse_input<P>(filename: P) -> io::Result<Vec<i32>>
+where
+    P: AsRef<Path>,
+{
+    if let Ok(lines) = read_lines(filename) {
+        let lines_as_strings: io::Result<Vec<String>> = lines.collect();
+        match lines_as_strings {
+            Ok(result) => {
+                let casts: Vec<i32> = result
+                    .iter()
+                    .map(|value| value.parse::<i32>().unwrap())
+                    .collect::<Vec<i32>>();
+                return Ok(casts);
+            }
+            Err(e) => {
+                return Err(e);
+            }
+        }
+    } else {
+        return Ok(Vec::new());
+    }
 }
